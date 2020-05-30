53aaef20e7a7db9f456c8cbcb2cd4bc110f3c90c
jonathandrumond03 <jonathandrumond03@gmail.com>
2020-05-30 14:49:57 -0300

    First Commit jonathan

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from './src/screens/Login';
@@ -9,13 +10,21 @@ import Chat from './src/screens/Chat';
 
 const Stack = createStackNavigator();
 
-export default function MyStack() {
+function StackNavigation(){
+  return(
+ <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Chat" component={Chat} />
+  </Stack.Navigator> 
+  );
+}
+
+export default function MainNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Chat" component={Chat} />
-      </Stack.Navigator>
+     <StackNavigation />
     </NavigationContainer>
   );
 }
+
+
--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -4,12 +4,13 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import { Container, Circle } from './styles';
 
+
 function Login({ navigation }) {
   return (
     <Container>
       <Circle />
       <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
-        <Text>Go to chat</Text>
+        <Text>Hello World!</Text>
       </TouchableOpacity>
     </Container>
   );
